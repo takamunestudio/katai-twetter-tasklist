ca7945180233b05a9f5ec041f441001246fb074e
takamunestudio <takamunetsuboi@gmail.com>
2020-07-29 17:41:53 +0000

    kadai-Twitter-Tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,9 @@ class TasksController < ApplicationController
   
   include SessionsHelper
   
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @tasks = current_user.tasks
   end
@@ -51,11 +50,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    # @task = Task.find(params[:id])
-    @task = current_user.tasks.find(params[:id])
-  end
   
   # Strong Parameter
   def task_params
